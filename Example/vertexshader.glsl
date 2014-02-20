aea38a4e582a1f724cd1babbf7dd483a8f409742
brownje <brownje@clarkson.edu>
2014-02-19 19:20:51 -0500

    Added a cool chest model and made it render.

--- Example/vertexshader.glsl
+++ Example/vertexshader.glsl
@@ -11,5 +11,6 @@ out vec4 pass_color;
 
 void main(){
   gl_Position=Matrix*viewMatrix*modelMatrix*vec4(in_position,1.0); 
-  pass_color=in_color;
+  pass_color=vec4(1.0, (0.5 + in_position.y), 1 - (0.5 + in_position.z), 1.0);
+  //pass_color=in_color;
 }
